dffb3178bf621765922ebfc107f17d7c873c3a9a
Marc-André Dumas <madumas@madumas.ca>
2023-02-07 15:59:48 -0500

    add avg error rate

--- health.js
+++ health.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 const N = parseInt(process.env.BLOCKS_BEHIND_THRESHOLD) || 100;
 const M = parseInt(process.env.RESPONSE_TIME_THRESHOLD) || 500;
+const Y = process.env.ERROR_RATE_LOOKBACK || 10;
+const errorThreshold = process.env.ERROR_THRESHOLD || 10; // 10% error rate
 const pollInterval = parseInt(process.env.POLL_INTERVAL) || 10000; // 10 seconds
 const indexerServiceUrl = process.env.INDEXER_SERVICE_URL || "https://api.thegraph.com/";
 const subgraphDeployment = process.env.SUBGRAPH_DEPL || "QmV614UpBCpuusv5MsismmPYu4KqLtdeNMKpiNrX56kw6u";
@@ -16,15 +18,26 @@ const subgraphHost = process.env.SUBGRAPH_HOST || "api.thegraph.com";
 const rpcURL = process.env.RPC_URL || "https://mainnet.infura.io/v3/your-infura-project-id";
 const bearerToken = process.env.BEARER_TOKEN;
 let healthStatus = "unknown";
+let errorRates = [];
 
 // Prometheus metrics
 // Create a Registry which registers the metrics
 const register = new Prometheus.Registry()
 const deltaMetric = new Prometheus.Gauge({ name: 'block_number_delta', help: 'Block number delta between Infura and The Graph' });
 const responseTimeMetric = new Prometheus.Gauge({ name: 'response_time', help: 'Response time in milliseconds' });
+const errorRateMetric = new Prometheus.Gauge({ name: 'error_rate', help: 'Average error rate for the past Y queries' });
 
 register.registerMetric(deltaMetric);
 register.registerMetric(responseTimeMetric);
+register.registerMetric(errorRateMetric);
+
+const calculateAverageErrorRate = () => {
+    let total = 0;
+    for (let i = 0; i < errorRates.length; i++) {
+        total += errorRates[i];
+    }
+    return total / errorRates.length;
+};
 
 const checkHealth = async () => {
     try {
@@ -53,19 +66,36 @@ const checkHealth = async () => {
 
         console.log(`Subgraph block number: ${subgraphBlockNumber}, Infura block number: ${infuraBlockNumber}, Delta: ${delta}, Response time: ${responseTime}ms`);
         if (delta > N || responseTime > M) {
-            healthStatus = "unhealthy";
+            errorRates.push(1);
         } else {
-            healthStatus = "healthy";
+            errorRates.push(0);
         }
+        if (errorRates.length > Y) {
+            errorRates.shift();
+        }
+
         deltaMetric.set(delta);
         responseTimeMetric.set(responseTime);
 
     } catch (error) {
         console.error(error);
-        healthStatus = "unhealthy";
+        errorRates.push(1);
+        if (errorRates.length > Y) {
+            errorRates.shift();
+        }
         deltaMetric.set(-1);
         responseTimeMetric.set(-1);
 
+    } finally {
+        const averageErrorRate = calculateAverageErrorRate();
+        console.log(`Average error rate for the past ${Y} queries: ${averageErrorRate}`);
+
+        if (averageErrorRate * 100 > errorThreshold) {
+            healthStatus = "unhealthy";
+        } else {
+            healthStatus = "healthy";
+        }
+        errorRateMetric.set(averageErrorRate * 100);
     }
 };
 

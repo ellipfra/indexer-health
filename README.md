b4e6ea2dcd61b1ed8537c7b5c3f9f4241410a3a3
Marc-André Dumas <madumas@madumas.ca>
2023-03-22 13:21:48 -0400

    multichain support

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 ## Indexer Health Check
 
-This is a simple health check to ensure that the indexer is running and indexing blocks. If the indexer is not running, the health check will fail.
+This is a simple health check to ensure that the indexer is running and indexing blocks for multiple chains. If the indexer is not running, the health check will fail.
 
 It checks for the response time, and number of blocks behind.
 
 ## Environment Variables
 
-The following environment variables are used by the health check script:
-- SUBGRAPH_DEPL: The name of the subgraph to check
+Create a `.env` file in the project root directory with the following environment variables. Use the `.env.example` file as a reference:
+
+- BEARER_TOKEN: The bearer token to use for authentication. E.g. your_bearer_token
+- POLL_INTERVAL: The interval in milliseconds between health checks. E.g. 10000
 - INDEXER_SERVICE_URL: The URL of the indexer service. E.g. http://localhost:80
-- POLL_INTERVAL: The interval in seconds between health checks. E.g. 60
-- RPC_URL: The URL of a reference JSON-RPC endpoint. E.g. http://localhost:8545
-- BLOCKS_BEHIND_THRESHOLD: The number of blocks behind the indexer can be before the health check fails. E.g. 100
-- RESPONSE_TIME_THRESHOLD: The maximum response time of the indexer in milliseconds before the health check fails. E.g. 1000
-- BEARER_TOKEN: The bearer token to use for authentication. E.g. 1234567890
+- SUBGRAPH_HOST: The host for the subgraph. E.g. your_subgraph_host
+
+For each chain, configure the following variables with the chain's prefix (e.g., CHAIN1_, CHAIN2_):
+
+- CHAINX_NAME: The name of the chain to check
+- CHAINX_BLOCKS_BEHIND_THRESHOLD: The number of blocks behind the indexer can be before the health check fails. E.g. 100
+- CHAINX_RESPONSE_TIME_THRESHOLD: The maximum response time of the indexer in milliseconds before the health check fails. E.g. 500
+- CHAINX_ERROR_RATE_LOOKBACK: The number of queries to look back for calculating the error rate. E.g. 10
+- CHAINX_ERROR_THRESHOLD: The error threshold percentage to consider the chain unhealthy. E.g. 10
+- CHAINX_SUBGRAPH_DEPL: The subgraph deployment ID for the chain. E.g. chain1_subgraph_deployment_id
+- CHAINX_RPC_URL: The URL of a reference JSON-RPC endpoint for the chain. E.g. chain1_rpc_url
 
 ## Usage
 
--- health.js
+++ health.js
@@ -7,18 +7,21 @@ const Prometheus = require('prom-client');
 
 dotenv.config();
 
-const N = parseInt(process.env.BLOCKS_BEHIND_THRESHOLD) || 100;
-const M = parseInt(process.env.RESPONSE_TIME_THRESHOLD) || 500;
-const Y = process.env.ERROR_RATE_LOOKBACK || 10;
-const errorThreshold = process.env.ERROR_THRESHOLD || 10; // 10% error rate
-const pollInterval = parseInt(process.env.POLL_INTERVAL) || 10000; // 10 seconds
-const indexerServiceUrl = process.env.INDEXER_SERVICE_URL || "https://api.thegraph.com/";
-const subgraphDeployment = process.env.SUBGRAPH_DEPL || "QmV614UpBCpuusv5MsismmPYu4KqLtdeNMKpiNrX56kw6u";
-const subgraphHost = process.env.SUBGRAPH_HOST || "api.thegraph.com";
-const rpcURL = process.env.RPC_URL || "https://mainnet.infura.io/v3/your-infura-project-id";
-const bearerToken = process.env.BEARER_TOKEN;
-let healthStatus = "unknown";
-let errorRates = [];
+const getConfig = (chainPrefix) => ({
+    N: parseInt(process.env[chainPrefix + "BLOCKS_BEHIND_THRESHOLD"]) || 100,
+    M: parseInt(process.env[chainPrefix + "RESPONSE_TIME_THRESHOLD"]) || 500,
+    Y: process.env[chainPrefix + "ERROR_RATE_LOOKBACK"] || 10,
+    errorThreshold: process.env[chainPrefix + "ERROR_THRESHOLD"] || 10,
+    pollInterval: parseInt(process.env.POLL_INTERVAL) || 10000,
+    indexerServiceUrl: process.env.INDEXER_SERVICE_URL || "https://api.thegraph.com/",
+    subgraphDeployment: process.env[chainPrefix + "SUBGRAPH_DEPL"] || "QmV614UpBCpuusv5MsismmPYu4KqLtdeNMKpiNrX56kw6u",
+    subgraphHost: process.env.SUBGRAPH_HOST || "api.thegraph.com",
+    rpcURL: process.env[chainPrefix + "RPC_URL"] || "https://mainnet.infura.io/v3/your-infura-project-id",
+    bearerToken: process.env.BEARER_TOKEN,
+    name: process.env[chainPrefix + "NAME"] || "unknown"
+});
+
+let healthStatuses = {};
 
 // Prometheus metrics
 // Create a Registry which registers the metrics
@@ -31,7 +34,7 @@ register.registerMetric(deltaMetric);
 register.registerMetric(responseTimeMetric);
 register.registerMetric(errorRateMetric);
 
-const calculateAverageErrorRate = () => {
+const calculateAverageErrorRate = (errorRates) => {
     let total = 0;
     for (let i = 0; i < errorRates.length; i++) {
         total += errorRates[i];
@@ -39,23 +42,24 @@ const calculateAverageErrorRate = () => {
     return total / errorRates.length;
 };
 
-const checkHealth = async () => {
+const checkHealth = async (config, errorRates) => {
     try {
-        console.log(`Checking health status...`)
+        console.log(`[${config.name}] Checking health status...`);
+
         const startTime = Date.now();
 
-        const subgraphResponse = await axios.post(indexerServiceUrl+"/subgraphs/id/"+subgraphDeployment, {
+        const subgraphResponse = await axios.post(config.indexerServiceUrl+"/subgraphs/id/"+config.subgraphDeployment, {
             query: '{_meta{block{number}}}'
         }, {
             headers: {
-                Authorization: `Bearer ${bearerToken}`,
-                Host: subgraphHost
+                Authorization: `Bearer ${config.bearerToken}`,
+                Host: config.subgraphHost
             }
         });
         const responseTime = Date.now() - startTime;
 
         const subgraphBlockNumber = JSON.parse(subgraphResponse.data.graphQLResponse).data._meta.block.number;
-        const { data: infuraBlockNumberResponse } = await axios.post(rpcURL, {
+        const { data: infuraBlockNumberResponse } = await axios.post(config.rpcURL, {
             jsonrpc: "2.0",
             id: 1,
             method: "eth_blockNumber",
@@ -64,13 +68,14 @@ const checkHealth = async () => {
         const infuraBlockNumber = parseInt(infuraBlockNumberResponse.result,16);
         const delta = Math.abs(parseInt(subgraphBlockNumber) - infuraBlockNumber);
 
-        console.log(`Subgraph block number: ${subgraphBlockNumber}, Infura block number: ${infuraBlockNumber}, Delta: ${delta}, Response time: ${responseTime}ms`);
-        if (delta > N || responseTime > M) {
+        console.log(`[${config.name}] Subgraph block number: ${subgraphBlockNumber}, Infura block number: ${infuraBlockNumber}, Delta: ${delta}, Response time: ${responseTime}ms`);
+
+        if (delta > config.N || responseTime > config.M) {
             errorRates.push(1);
         } else {
             errorRates.push(0);
         }
-        if (errorRates.length > Y) {
+        if (errorRates.length > config.Y) {
             errorRates.shift();
         }
 
@@ -78,33 +83,35 @@ const checkHealth = async () => {
         responseTimeMetric.set(responseTime);
 
     } catch (error) {
-        console.error(error);
+        console.error(`[${config.name}]`, error);
         errorRates.push(1);
-        if (errorRates.length > Y) {
+        if (errorRates.length > config.Y) {
             errorRates.shift();
         }
         deltaMetric.set(-1);
         responseTimeMetric.set(-1);
 
     } finally {
-        const averageErrorRate = calculateAverageErrorRate();
-        console.log(`Average error rate for the past ${Y} queries: ${averageErrorRate}`);
+        const averageErrorRate = calculateAverageErrorRate(errorRates);
+        console.log(`[${config.name}] Average error rate for the past ${config.Y} queries: ${averageErrorRate}`);
 
-        if (averageErrorRate * 100 > errorThreshold) {
+        if (averageErrorRate * 100 > config.errorThreshold) {
             healthStatus = "unhealthy";
         } else {
             healthStatus = "healthy";
         }
         errorRateMetric.set(averageErrorRate * 100);
     }
+    return healthStatus;
 };
 
 app.get('/health', (req, res) => {
-    console.log(`Received /health request, health status: ${healthStatus}`);
-    if (healthStatus === "healthy") {
+    console.log(`Received /health request, health statuses:`, healthStatuses);
+    const unhealthyChains = Object.entries(healthStatuses).filter(([_, status]) => status === 'unhealthy');
+    if (unhealthyChains.length === 0) {
         res.status(200).send("healthy");
     } else {
-        res.status(503).send("unhealthy");
+        res.status(503).send(`unhealthy: ${unhealthyChains.map(([chain]) => chain).join(', ')}`);
     }
 });
 
@@ -122,5 +129,27 @@ prom_app.get('/metrics', (req, res) => {
     });
 });
 
-checkHealth().then();
-setInterval(checkHealth, pollInterval);
+const startMonitoring = (chainPrefix) => {
+    const config = getConfig(chainPrefix);
+    const errorRates = [];
+    const healthCheck = async () => {
+        healthStatuses[config.name] = await checkHealth(config, errorRates);
+        setTimeout(healthCheck, config.pollInterval);
+    };
+    healthCheck().then();
+};
+
+const getChains = () => {
+    let chainPrefixes = [];
+    let i = 1;
+    while (process.env[`CHAIN${i}_NAME`]) {
+        chainPrefixes.push(`CHAIN${i}_`);
+        i++;
+    }
+    return chainPrefixes;
+};
+
+const chains = getChains(); // This will return an array of configured chain prefixes
+
+chains.forEach(startMonitoring);
+
